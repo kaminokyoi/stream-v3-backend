618d52c76faea4fda79ffd4b92617241c82b0287
wonder7b <matutamedi@gmail.com>
2026-07-20 22:08:33 +0100

    Add web-admin url to API CorsHeader

--- config/settings.py
+++ config/settings.py
@@ -60,6 +60,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
     'http://127.0.0.1:3001',
     'http://127.0.0.1:8081',
+    'https://cadmin.streampartner.in',
 ]
 _extra_cors = os.getenv('CORS_ALLOWED_ORIGINS', '').strip()
 if _extra_cors:
